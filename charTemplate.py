031683a37475078c7aee0cc07bb8b31b6ba02b98
Joshua Wood <JerichoWall@apu.edu>
2020-04-18 13:13:20 -0700

    Basic character template

---
+++ charTemplate.py
@@ -0,0 +1,14 @@
+
+class Character():
+    def __init__(self, life, mana, might, agility, vigor, intellect, savvy, serendipity, charType, special, name):
+        self.life = life
+        self.mana = mana
+        self.might = might
+        self.agility = agility
+        self.vigor = vigor
+        self.intellect = intellect
+        self.savvy = savvy
+        self.serendipity = serendipity
+        self.charType = charType
+        self.special = special
+        self.name = name
